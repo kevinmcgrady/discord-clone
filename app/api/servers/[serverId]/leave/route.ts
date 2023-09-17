496a166863339e904d153ad4b56bc44ce3b01139
Kev Mcgrady <kevinmcgrady47@gmail.com>
2023-09-17 15:47:03 +0100

    feat/leave server

---
+++ app/api/servers/[serverId]/leave/route.ts
@@ -0,0 +1,49 @@
+import { NextResponse } from 'next/server';
+
+import { currentProfile } from '@/lib/current-profile';
+import { db } from '@/lib/db';
+
+type Params = {
+  params: {
+    serverId: string;
+  };
+};
+
+export async function PATCH(req: Request, { params }: Params) {
+  try {
+    const profile = await currentProfile();
+
+    if (!profile) {
+      return new NextResponse('Unauthorized', { status: 401 });
+    }
+
+    if (!params.serverId) {
+      return new NextResponse('Server id missing', { status: 400 });
+    }
+
+    const server = await db.server.update({
+      where: {
+        id: params.serverId,
+        profileId: {
+          not: profile.id,
+        },
+        members: {
+          some: {
+            profileId: profile.id,
+          },
+        },
+      },
+      data: {
+        members: {
+          deleteMany: {
+            profileId: profile.id,
+          },
+        },
+      },
+    });
+
+    return NextResponse.json(server);
+  } catch (error) {
+    return new NextResponse('Internal Error', { status: 500 });
+  }
+}

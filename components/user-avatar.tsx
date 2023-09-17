da9867de2471eafdae9cd2286f1dfa4ab8aa48e2
Kev Mcgrady <kevinmcgrady47@gmail.com>
2023-09-17 13:11:00 +0100

    feat/manage members: added change role and kick member

---
+++ components/user-avatar.tsx
@@ -0,0 +1,15 @@
+import { Avatar, AvatarImage } from '@/components/ui/avatar';
+import { cn } from '@/lib/utils';
+
+type UserAvatarProps = {
+  src?: string;
+  classname?: string;
+};
+
+export const UserAvatar = ({ classname, src }: UserAvatarProps) => {
+  return (
+    <Avatar className={cn('h-7 w-7 md:h-10 md:w-10', classname)}>
+      <AvatarImage src={src} />
+    </Avatar>
+  );
+};
